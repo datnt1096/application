e6c9fee0d638743564182a58229b09f56f191e71
Dat Nguyen <nguyen.thanh.datc@sun-asterisk.com>
2019-12-03 15:05:12 +0700

    config user info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
 
   def logged_in_user
     return if user_signed_in?
-    store_location
-    flash[:danger] = t ".require_login"
-    redirect_to login_path
+
+    store_location_for(:user, request.fullpath)
+    flash[:danger] = t "users.require_login"
+    redirect_to new_user_session_path
   end
 end
